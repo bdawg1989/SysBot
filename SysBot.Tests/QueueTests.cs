f3d45b009d77061506d1b03e032aff55a40483f7
Secludedly <87262197+Secludedly@users.noreply.github.com>
2024-04-14 22:12:20 -0400

    Bot Status Update

--- SysBot.Tests/QueueTests.cs
+++ SysBot.Tests/QueueTests.cs
@@ -87,7 +87,6 @@ public class QueueTests
         public override Task MainLoop(CancellationToken token) => Task.CompletedTask;
         public override void SoftStop() { }
         public override Task HardStop() => Task.CompletedTask;
-
         public override Task<T> ReadPokemon(ulong offset, CancellationToken token) => Task.Run(() => new T(), token);
         public override Task<T> ReadPokemon(ulong offset, int size, CancellationToken token) => Task.Run(() => new T(), token);
         public override Task<T> ReadPokemonPointer(IEnumerable<long> jumps, int size, CancellationToken token) => Task.Run(() => new T(), token);
